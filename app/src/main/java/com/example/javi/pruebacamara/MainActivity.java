736ac199054bca9434903457a5f86da97a64e63d
Javi <jvano@iesfbmoll.org>
2018-11-12 17:20:00 +0100

    Request Camera Permision Added

--- app/src/main/java/com/example/javi/pruebacamara/MainActivity.java
+++ app/src/main/java/com/example/javi/pruebacamara/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.javi.pruebacamara;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +16,8 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imageView;
+    private ImageView imageView;
+    private int x = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent,0);
             }
         });
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, x);
+        }
     }
 
     @Override
